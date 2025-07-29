e12de23a2f5a4a31c165f9bb37d48d8c6ca5c954
Andrei Onel <onel@users.noreply.github.com>
2025-07-29 15:51:56 +0300

    Added reference documentation for: src/renderer/components/Spinner.tsx

--- src/renderer/components/Spinner.tsx
+++ src/renderer/components/Spinner.tsx
@@ -1,6 +1,16 @@
 import { forwardRef } from 'react';
 import './Spinner.scss';
 
+/**
+ * A loading spinner component that displays an animated spinning indicator.
+ * The component renders four div elements that are styled via CSS to create a spinning animation.
+ * 
+ * @param props - The component props
+ * @param props.size - The width and height of the spinner in pixels. Defaults to 24.
+ * @param props.className - Additional CSS classes to apply to the spinner container. Defaults to empty string.
+ * @param ref - React ref that will be forwarded to the spinner container div element
+ * @returns A div element containing four child divs that form the spinning animation
+ */
 function Spinner(
   {
     size = 24,
