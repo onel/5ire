520d90061907df7a262390b6b89d8206ced52abe
Andrei Onel <onel@users.noreply.github.com>
2025-07-29 15:51:18 +0300

    Added reference documentation for: .erb/scripts/link-modules.ts

--- .erb/scripts/link-modules.ts
+++ .erb/scripts/link-modules.ts
@@ -1,9 +1,20 @@
+/**
+ * Module linking utility script that creates symbolic links between node_modules directories.
+ * This script ensures that the source directory has access to node_modules by creating
+ * a junction link when necessary.
+ */
+
 import fs from 'fs';
 import webpackPaths from '../configs/webpack.paths';
 
 const { srcNodeModulesPath } = webpackPaths;
 const { appNodeModulesPath } = webpackPaths;
 
+/**
+ * Creates a symbolic link (junction) from appNodeModulesPath to srcNodeModulesPath
+ * if the source node_modules doesn't exist but the app node_modules does.
+ * This ensures the source directory can access dependencies through the symbolic link.
+ */
 if (!fs.existsSync(srcNodeModulesPath) && fs.existsSync(appNodeModulesPath)) {
   fs.symlinkSync(appNodeModulesPath, srcNodeModulesPath, 'junction');
 }
