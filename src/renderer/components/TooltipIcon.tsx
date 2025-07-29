00521473c0c14f6623213a074b40154da316cfc8
Andrei Onel <onel@users.noreply.github.com>
2025-07-29 15:52:03 +0300

    Added reference documentation for: src/renderer/components/TooltipIcon.tsx

--- src/renderer/components/TooltipIcon.tsx
+++ src/renderer/components/TooltipIcon.tsx
@@ -2,6 +2,15 @@ import { PositioningShorthand, Tooltip } from '@fluentui/react-components';
 import { Info16Regular } from '@fluentui/react-icons';
 import { ReactNode } from 'react';
 
+/**
+ * Renders an information icon with a tooltip that displays additional content when hovered or focused.
+ * The component conditionally renders based on whether tip content is provided.
+ * 
+ * @param props - The component props
+ * @param props.tip - The content to display in the tooltip. Can be a string or React node. If null/undefined, no tooltip is rendered.
+ * @param props.positioning - The positioning of the tooltip relative to the icon. Defaults to 'above-start'.
+ * @returns A tooltip with an info icon if tip content is provided, otherwise null
+ */
 export default function TooltipIcon({
   tip,
   positioning = 'above-start',
