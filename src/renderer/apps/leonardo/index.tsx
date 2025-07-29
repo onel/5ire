60f0ba4f50668b49d28c72ed9d3bed91be758fe8
Andrei Onel <onel@users.noreply.github.com>
2025-07-29 15:51:09 +0300

    Added reference documentation for: src/renderer/apps/leonardo/index.tsx

--- src/renderer/apps/leonardo/index.tsx
+++ src/renderer/apps/leonardo/index.tsx
@@ -1,5 +1,9 @@
 import icon from './app-leonardo.png';
 
+/**
+ * Leonardo app configuration object
+ * Defines the basic properties and settings for the Leonardo image generation app
+ */
 export default {
   name: 'Leonardo',
   description: 'Leonardo generates images from natural language descriptions',
